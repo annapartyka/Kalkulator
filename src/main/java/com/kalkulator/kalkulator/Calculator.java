e642c32940fe5c5d3cf3468712ef23b27de605d0
annapartyka <anna.katarzyna.partyka@gmail.com>
2018-02-11 00:19:43 +0100

    Calculator

--- src/main/java/com/kalkulator/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/kalkulator/Calculator.java
@@ -9,4 +9,13 @@ public class Calculator {
         return a - b;
     }
 
+    public void someMethod(){
+        System.out.println("It works.");
+    }
+
+    public static void main (String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+
+    }
 }
